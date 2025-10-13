ad501877d86240813b72f20a0032a99bacbfb561
bruce wyllis <brucewyllis01@gmail.com>
2025-10-13 15:03:35 +0300

    adding a feature in the admin that allows for eaching of a product by category price e.t.c

--- Alx_CapstoneProject/products/admin.py
+++ Alx_CapstoneProject/products/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Product, Category
-admin.site.register(Product)
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "stock")
+    search_fields = ("name", "description")
+    list_filter = ("category",)
+    list_editable = ("price", "stock")
+
