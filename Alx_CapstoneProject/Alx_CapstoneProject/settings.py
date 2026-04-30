188b1e9fdc3ea92f8560b21a0c307750d9505cf2
bruce wyllis <brucewyllis01@gmail.com>
2026-04-30 22:27:31 +0300

    configure PostgreSQL, update Django settings, and migrate existing SQLite data

--- Alx_CapstoneProject/Alx_CapstoneProject/settings.py
+++ Alx_CapstoneProject/Alx_CapstoneProject/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURwhich python3ITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ebi-6)1_)#_69x7_jcf$f$=yo3)bf9ve^pifmm()p)$fzv=p!w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'Alx_CapstoneProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce_db',
+        'USER': 'ecommerce_user',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
